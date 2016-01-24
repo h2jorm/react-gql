2ad69301f8a53c36182a2ea2e9c501064db633d8
leeching <417300981@qq.com>
2016-01-24 21:57:39 +0800

    test: support sourcemap in karam

--- karma.conf.js
+++ karma.conf.js
@@ -27,11 +27,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.spec.js': ['webpack']
+      'test/*.spec.js': ['webpack', 'sourcemap']
     },
 
 
     webpack: {
+      devtool: 'inline-source-map',
       module: {
         loaders: [
           {
